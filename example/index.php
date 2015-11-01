65e4b39ebd71f10504a0234da9c64ae1beca1fb3
Prakash <thebhandariprakash@gmail.com>
2015-11-01 17:25:07 +0545

    new file pushed to github server

---
+++ example/index.php
@@ -0,0 +1,21 @@
+<?php
+require '../src/imageInfo/ImageInformation.php';
+use imageInfo\ImageInformation;
+
+
+$img = new ImageInformation();
+
+$imageInfo = $img->getInformation("../assets/img/test.jpg");
+if ($camera) {
+    echo '<img src="../assets/img/test.jpg" alt="" height="250" /> <br />';
+    echo "Camera Used: " . $imageInfo['model'] . "<br />";
+    echo "Maker: " . $imageInfo['maker'] . "<br />";
+    echo "Exposure Time: " . $imageInfo['exposure'] . "<br />";
+    echo "Aperture: " . $imageInfo['aperture'] . "<br />";
+    echo "ISO: " . $imageInfo['iso'] . "<br />";
+    echo "FocalLength: " . $imageInfo['focalLength'] . "<br />";
+    echo "Date Taken: " . $imageInfo['date'] . "<br />";
+} else {
+    echo '<span style="color: red;">Image not found</span>';
+
+}
---
+++ src/imageInfo/ImageInformation.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * @package     ImageInformation class
+ * @version     1.0
+ * @author      Prakash Bhandari (http://www.prakashbhandari.com.np)
+ * @license     This software is licensed under the MIT license: http://opensource.org/licenses/MIT
+ * @copyright   Prakash Bhandari (http://www.prakashbhandari.com.np)
+ */
+
+namespace imageInfo;
+
+class ImageInformation
+{
+    public $unavailable = "Info Unavailable";
+
+    function getInformation($imagePath)
+    {
+
+        if ((isset($imagePath)) and (file_exists($imagePath))) {
+
+            $ifdo = read_exif_data($imagePath, 'IFD0', 0);
+            $exif = read_exif_data($imagePath, 'EXIF', 0);
+
+            // Maker
+            if (@array_key_exists('Make', $ifdo)) {
+                $maker = $ifdo['Make'];
+            } else {
+                $maker = $this->unavailable;
+            }
+
+            // Model
+            if (@array_key_exists('Model', $ifdo)) {
+                $model = $ifdo['Model'];
+            } else {
+                $model = $this->unavailable;
+            }
+
+            // Exposure
+            if (@array_key_exists('ExposureTime', $ifdo)) {
+                $exposure = $ifdo['ExposureTime'];
+            } else {
+                $exposure = $this->unavailable;
+            }
+
+            // Aperture
+            if (@array_key_exists('ApertureFNumber', $ifdo['COMPUTED'])) {
+                $aperture = $ifdo['COMPUTED']['ApertureFNumber'];
+            } else {
+                $aperture = $this->unavailable;
+            }
+
+            // Created Date
+            if (@array_key_exists('DateTime', $ifdo)) {
+                $createDate = $ifdo['DateTime'];
+            } else {
+                $createDate = $this->unavailable;
+            }
+
+            // ISO
+            if (@array_key_exists('ISOSpeedRatings', $exif)) {
+                $iso = $exif['ISOSpeedRatings'];
+            } else {
+                $iso = $this->unavailable;
+            }
+
+            // Focal Length
+            if (@array_key_exists('FocalLength', $exif)) {
+                $focalLength = $exif['FocalLength'];
+            } else {
+                $focalLength = $this->unavailable;
+            }
+
+            $infoArray = array();
+            $infoArray['maker'] = $maker;
+            $infoArray['model'] = $model;
+            $infoArray['exposure'] = $exposure;
+            $infoArray['aperture'] = $aperture;
+            $infoArray['date'] = $createDate;
+            $infoArray['iso'] = $iso;
+            $infoArray['focalLength'] = $focalLength;
+            return $infoArray;
+
+        } else {
+            return false;
+        }
+    }
+}
